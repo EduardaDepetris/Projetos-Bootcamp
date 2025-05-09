30ebb19ff0a36d8ce22e414041a72f6f5fe6ca1e
EduardaDepetris <dudinhadepetris04@gmail.com>
2025-05-09 12:15:56 -0300

    Style: Comentários adicionados

--- Desafio_um/Desafio_um.js
+++ Desafio_um/Desafio_um.js
@@ -1,3 +1,19 @@
+/*Descrição
+Você é um aventureiro e está prestes a embarcar em uma jornada de proporções épicas! 
+A cada desafio enfrentado, é crucial que você avalie com cuidado se possui tudo o que precisa para avançar, 
+pois até mesmo a falta de um simples artefato pode fazer toda a diferença entre a vitória e a derrota iminente. 
+No momento, em seu inventário, você possui uma espada, um escudo, seis poções de cura, quatro poções de mana e três pergaminhos. 
+Diante disso, é hora de você verificar se cada item essencial para o próximo desafio está disponível em seu inventário. 
+Assim, sua missão é criar um algoritmo que retorne a mensagem “Disponível”, 
+caso possua o item na quantidade especificada no seu inventário, e “Indisponível” caso não.
+
+Entrada
+A entrada vai receber duas informações: o nome do item a ser verificado e a quantidade desejada desse item.
+
+Saída
+Imprima "Disponível" se a quantidade desejada do item estiver disponível no inventário e "Indisponível" caso contrário. 
+*/
+
 
 const prompt = require('prompt-sync')();
 
